fa35ff0944e63fe313d09f74ef8c4ab4027265b6
val.istar.guo <val.istar.guo@gmail.com>
2021-04-05 01:08:50 +0800

    feat: middleware mount utils
    re #5

---
+++ src/mount.ts
@@ -0,0 +1,51 @@
+import { MiddlewareMatcher } from "./middleware";
+import * as micromatch from 'micromatch'
+import { Context } from "node:vm";
+
+interface Mounter extends MiddlewareMatcher {
+  pathname(matcher: string | RegExp): Mounter
+  location(): Mounter
+  host(host: string): Mounter
+  // (matcher: string | RegExp): MiddlewareMatcher
+}
+
+
+function createMounter(matcher: MiddlewareMatcher): Mounter {
+  const mounter: Mounter = ctx => matcher(ctx)
+  mounter.pathname = arg => compose(mounter, pathname(arg))
+  mounter.location = () => compose(mounter, location())
+  mounter.host = arg => compose(mounter, host(arg))
+
+  return mounter
+}
+
+
+export function location(): Mounter {
+  return createMounter((ctx: Context) => {
+    return !ctx.request.url.host
+  })
+}
+
+export function pathname(matcher: string | RegExp): Mounter {
+  return createMounter(ctx => {
+    const pathname = ctx.url.pathname || '/'
+    if (typeof matcher === 'string') return micromatch.isMatch(pathname, matcher)
+    return matcher.test(pathname)
+  })
+}
+
+export function host(host: string): Mounter {
+  return createMounter(ctx => ctx.url.host === host)
+}
+
+
+function compose(...mounters: Mounter[]): Mounter {
+  return createMounter(ctx => {
+    for (const mounter of mounters) {
+      if (mounter(ctx)) continue
+      return false
+    }
+
+    return true
+  })
+}
