1c00f0e3bc7ab5d661d9aa64c033cfb644d3b054
val.istar.guo <val.istar.guo@gmail.com>
2022-05-01 14:12:16 +0800

    fix: global middleware is polluted

--- src/util/create-request.ts
+++ src/util/create-request.ts
@@ -6,7 +6,6 @@ import {
   MiddlewareMatcher,
   Options,
 } from '@/types'
-import * as clone from 'clone'
 import { matchMiddleware, matchHost } from '../middleware'
 import { KeqURL } from '../keq-url'
 
@@ -17,40 +16,40 @@ export function createRequest(): RequestCreator {
   const middlewares: Middleware[] = []
 
   const request: RequestCreator = function<T>(params: RequestParams, options: Options = defaultOptions): ReturnType<Keq<T>['options']> {
-    const request = new Keq<T>(new KeqURL(params.url), 'get', middlewares)
+    const request = new Keq<T>(new KeqURL(params.url), 'get', [...middlewares])
     return request.options(options)
   }
 
 
   request.get = function<T>(href: string): Keq<T> {
-    const request = new Keq<T>(new KeqURL(href), 'get', clone(middlewares))
+    const request = new Keq<T>(new KeqURL(href), 'get', [...middlewares])
     return request
   }
 
   request.put = function<T>(href: string): Keq<T> {
-    const request = new Keq<T>(new KeqURL(href), 'put', middlewares)
+    const request = new Keq<T>(new KeqURL(href), 'put', [...middlewares])
     return request
   }
 
   request.delete = function<T>(href: string): Keq<T> {
-    const request = new Keq<T>(new KeqURL(href), 'delete', middlewares)
+    const request = new Keq<T>(new KeqURL(href), 'delete', [...middlewares])
     return request
   }
 
   request.del = request.delete
 
   request.post = function<T>(href: string): Keq<T> {
-    const request = new Keq<T>(new KeqURL(href), 'post', middlewares)
+    const request = new Keq<T>(new KeqURL(href), 'post', [...middlewares])
     return request
   }
 
   request.head = function<T>(href: string): Keq<T> {
-    const request = new Keq<T>(new KeqURL(href), 'head', middlewares)
+    const request = new Keq<T>(new KeqURL(href), 'head', [...middlewares])
     return request
   }
 
   request.patch = function<T>(href: string): Keq<T> {
-    const request = new Keq<T>(new KeqURL(href), 'patch', middlewares)
+    const request = new Keq<T>(new KeqURL(href), 'patch', [...middlewares])
     return request
   }
 
