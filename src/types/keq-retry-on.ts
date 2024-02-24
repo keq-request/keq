779d9a69beec022099c8d5dc65624dd3fb2000ea
val.istar.guo <val.istar.guo@gmail.com>
2024-02-24 19:29:04 +0800

    chore: upgrade denpendencies

--- src/types/keq-retry-on.ts
+++ src/types/keq-retry-on.ts
@@ -1,3 +1,4 @@
 import { KeqContext } from './keq-context'
 
+// eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents
 export type KeqRetryOn = (attempt: number, error: unknown | null, ctx: KeqContext) => boolean
