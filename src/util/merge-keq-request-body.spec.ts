eb422d5e9813b8be9229bdf7ac947ddc67fb857a
val.istar.guo <val.istar.guo@gmail.com>
2024-10-25 10:44:18 +0800

    test: add mergeKeqRequestBody test case

--- src/util/merge-keq-request-body.spec.ts
+++ src/util/merge-keq-request-body.spec.ts
@@ -14,18 +14,26 @@ test('mergeKeqResponseBody(any, object)', () => {
   expect(mergeKeqRequestBody([1, 2, 3], { a: 1 })).toEqual({ a: 1 })
   expect(mergeKeqRequestBody('abc', { a: 1 })).toEqual({ a: 1 })
   expect(mergeKeqRequestBody({ a: 1 }, { b: 2 })).toEqual({ a: 1, b: 2 })
+})
 
+test('mergeKeqResponseBody(any, FormData)', () => {
   const formData = new FormData()
   formData.append('a', 'a1')
   formData.append('b', 'b1')
   formData.append('b', 'b2')
-  expect(mergeKeqRequestBody({ a: 'a0', b: 'b0', c: 'c0' }, formData)).toEqual({ a: 'a1', b: ['b1', 'b2'], c: 'c0' })
 
+  expect(mergeKeqRequestBody(undefined, formData)).toEqual({ a: 'a1', b: ['b1', 'b2'] })
+  expect(mergeKeqRequestBody({ a: 'a0', b: 'b0', c: 'c0' }, formData)).toEqual({ a: 'a1', b: ['b1', 'b2'], c: 'c0' })
+})
+
+test('mergeKeqResponseBody(any, URLSearchParams)', () => {
   const urlSearchParams = new URLSearchParams()
   urlSearchParams.append('a', 'a1')
   urlSearchParams.append('b', 'b1')
   urlSearchParams.append('b', 'b2')
+
   expect(mergeKeqRequestBody({ a: 'a0', b: 'b0', c: 'c0' }, urlSearchParams)).toEqual({ a: 'a1', b: ['b1', 'b2'], c: 'c0' })
+  expect(mergeKeqRequestBody(undefined, urlSearchParams)).toEqual({ a: 'a1', b: ['b1', 'b2'] })
 })
 
 test('mergeKeqRequestBody(any, string)', () => {
